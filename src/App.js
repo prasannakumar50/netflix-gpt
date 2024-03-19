c80d1faeeaf6fa867fbf02cf8a5b335d505dc639
prasannakumar50 <babblu293@gmail.com>
2024-03-19 17:41:45 +0530

    first commit
    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Namastey EveryOne, Lets Build Netflix-GPT
+    <div className="App text -3xl font-bold">
+         Hello Everyone .... Lets Build Netfilx- gpt....
 
     </div>
   );
