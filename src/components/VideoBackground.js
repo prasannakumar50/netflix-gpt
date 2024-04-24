93b55d83fd065c4947e91463efd483da6c765e87
prasannakumar50 <babblu293@gmail.com>
2024-04-24 12:19:37 +0530

    updated the videobackground component

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,32 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { API_OPTIONS } from '../constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTrailerVideo } from "../utils/moviesSlice";
 
-const VideoBackground = ({movieId}) => {
-
-    const getMovieVideos = async()=>{
-
-     const data = await fetch('https://api.themoviedb.org/3/movie/823464/videos?language=en-US',API_OPTIONS)
-     const json = await data.json();
-     console.log(json)
-      
-     const filterData = json.results.filter(video => video.type === "Trailer")
-     const trailer = filterData.length ? filterData[0] : json.results[0]
-     console.log(trailer)
-
-    }
-
-    useEffect(()=>{
-        getMovieVideos();
-    },[])
-     
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
+  const dispatch = useDispatch();
 
 
-    
+ //fetching the trailer video and updating the store with trailer video
+  const getMovieVideos = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/823464/videos?language=en-US', API_OPTIONS);
+    const json = await data.json();
+    const filterData = json.results.filter(video => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  }
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+
+
+
   return (
     <div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/XeDbyVODQ5M?si=DTQC9hdr6MePIqM3" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default VideoBackground
+export default VideoBackground;
