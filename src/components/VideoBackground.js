08d569ff814769cf303a875590e17dbbd5f12a03
prasannakumar50 <babblu293@gmail.com>
2024-04-24 18:06:28 +0530

    TMDB fetch data integration && Main Container

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,34 +1,18 @@
-import React, { useEffect } from 'react';
-import { API_OPTIONS } from '../constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTrailerVideo } from "../utils/moviesSlice";
+
+import {  useSelector } from 'react-redux';
+import useMovieTrailer from '../hooks/useMovieTrailer';
 
 const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-  const dispatch = useDispatch();
-
-
- //fetching the trailer video and updating the store with trailer video
-  const getMovieVideos = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/823464/videos?language=en-US', API_OPTIONS);
-    const json = await data.json();
-    const filterData = json.results.filter(video => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
-  }
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
-
-
+  
+  useMovieTrailer(movieId);
 
   return (
-    <div>
+    <div className='w-screen'>
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+         className='w-screen aspect-video'
+         
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { API_OPTIONS } from "../constants";
+import { useEffect } from "react";
+
+
+const useMovieTrailer = (movieId) =>{
+    const dispatch = useDispatch();
+
+
+    //fetching the trailer video and updating the store with trailer video
+     const getMovieVideos = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/'+movieId+'/videos?language=en-US', API_OPTIONS)
+       
+      
+       const json = await data.json();
+       
+       const filterData = json.results.filter(video => video.type === "Trailer");
+
+       const trailer = filterData.length ? filterData[0] : json.results[0];
+       dispatch(addTrailerVideo(trailer));
+     }
+   
+     useEffect(() => {
+       getMovieVideos();
+     }, []);
+   
+
+}
+
+export default useMovieTrailer;
