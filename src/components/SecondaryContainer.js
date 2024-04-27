c8bc1e2134a43f1fecfaf94799af82be14c6a6cb
prasannakumar50 <babblu293@gmail.com>
2024-04-27 17:13:28 +0530

    updated movie list component

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,19 +8,13 @@ const SecondaryContainer = () => {
   const movies = useSelector(store => store.movies)
 
   return (
-    <div>      
-    {/* 
-      MovieList - Popular
-       - Movie card * n 
-      MovieList - Trending
-      MovieList - thriller
-    
-    */}
+    <div className=' bg-black'>      
+       <div className='-mt-52 relative z-20 pl-12'>
         <MovieList title={"Now Playing"}  movies={movies.nowPlayingMovies}/>
         <MovieList title={"Trending"}  movies={movies.nowPlayingMovies}/>
         <MovieList title={"Popular"}  movies={movies.nowPlayingMovies}/>
         <MovieList title={"Horror"}  movies={movies.nowPlayingMovies}/>
-      
+        </div>
 
 
     </div>
