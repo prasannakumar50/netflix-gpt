d9c9109a8199b4dad14f24b657f3523dd514996c
prasannakumar50 <babblu293@gmail.com>
2024-04-29 12:10:55 +0530

    Added new hooks for the app

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -11,9 +11,9 @@ const SecondaryContainer = () => {
     <div className=' bg-black'>      
        <div className='-mt-52 relative z-20 pl-12'>
         <MovieList title={"Now Playing"}  movies={movies.nowPlayingMovies}/>
-        <MovieList title={"Trending"}  movies={movies.nowPlayingMovies}/>
-        <MovieList title={"Popular"}  movies={movies.nowPlayingMovies}/>
-        <MovieList title={"Horror"}  movies={movies.nowPlayingMovies}/>
+        <MovieList title={"Popular"}  movies={movies.popularMovies}/>
+        <MovieList title={"Top Rated"}  movies={movies.topRatedMovies}/>
+        <MovieList title={"Upcoming"}  movies={movies.upcomingMovies}/>
         </div>
 
 
---
+++ src/hooks/useTopRatedMovies.js
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import { addPopularMovies, addTopRatedMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+
+const useTopRatedMovies = ()=>{
+
+    //Fetch  Data from TMDB api
+    const dispatch = useDispatch(); 
+
+    const getTopRatedMovies = async()=>{
+        const data = await fetch('https://api.themoviedb.org/3/movie/top_rated?page=1',API_OPTIONS);
+        const json =await data.json();
+        console.log(json.results)
+        dispatch(addTopRatedMovies(json.results))
+    }
+
+      useEffect(()=>{
+        getTopRatedMovies();
+      },[])
+}
+
+export default useTopRatedMovies;
